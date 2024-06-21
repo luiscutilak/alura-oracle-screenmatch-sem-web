e724fbbd4e59cefadfe66dd282ba7d6aa68d486a
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-06-21 08:29:32 -0300

    feat:Utilizando sistema native query

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -89,7 +89,7 @@ public class Principal {
         System.out.println("Com avaliação a partir de qual valor? ");
         var avaliacao = leitura.nextDouble();
         leitura.nextLine();
-        List<Serie> filtroSeries = repositorio.findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(totalTemporadas, avaliacao);
+        List<Serie> filtroSeries = repositorio.seriesPorTemporadaEavaliacao();
         System.out.println("*** Séries filtradas ***");
         filtroSeries.forEach(s ->
                 System.out.println(s.getTitulo() + " - avaliação: " + s.getAvaliacao()));
